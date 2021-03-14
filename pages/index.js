07c5f7dd251c05bd6ff61a577bfe2901fbd1cd85
Chris Miller <miller.t.chris@gmail.com>
2021-03-13 22:42:22 -0500

    Animating hero headline

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import TypeIt from "typeit-react"
+import { Controls, PlayState, Timeline, Tween } from 'react-gsap';
 
 export default function Home() {
   return (
@@ -24,53 +25,102 @@ export default function Home() {
         <div className="wrapper">
           <div className="row">
             <div className="col">
-              <h1 class="title"><div>We create</div> 
-              
-                <div class="slash">
-                  <TypeIt
-                    getBeforeInit={instance => {
-                      instance
-                        .type("interactive")
-                        .pause(600)
-                        .delete(11)
-                        .type("bespoke")
-                        .pause(600)
-                        .delete(7)
-                        .type("reactive")
-                        .pause(600)
-                        .delete(8)
-                        .type("engaging")
-                        .pause(600)
-                        .delete(8)
-                        .type("intentional")
-                        .pause(600)
-                        .delete(11)
-                        .type("fun")
-                        .pause(600)
-                        .delete(3)
-                        .type("lovely")
-                        .pause(600)
-                        .delete(6)
-                        .pause(600)
-                        .type("interactive")
-                      // Remember to return it!
-                      return instance;
-                    }}
-                    options={{
-                      cursor: false,
-                      waitUntilVisible: true,
-                      loop: false
-                    }}>
-                  </TypeIt>
-                </div> 
+
+              <h1 class="title">
+                <Tween from={{ opacity: 0, scale: 0.5 }} to={{ opacity: 1, scale: 1 }} duration={4} ease="expo(1.7)">
+                  <div>We create</div> 
+                </Tween>
                 
-                <div>expereinces.</div>
+                <Tween from={{ opacity: 0, scale: 0.5 }} to={{ opacity: 1, scale: 1 }} duration={4} delay={0.3} ease="expo(1.7)">
+
+                  <div class="slash">
+                    <TypeIt
+                      getBeforeInit={instance => {
+                        instance
+                          .type("interactive")
+                          .pause(600)
+                          .delete(11)
+                          .type("bespoke")
+                          .pause(600)
+                          .delete(7)
+                          .type("accessibile")
+                          .pause(600)
+                          .delete(11)
+                          .type("reactive")
+                          .pause(600)
+                          .delete(8)
+                          .type("engaging")
+                          .pause(600)
+                          .delete(8)
+                          .type("intentional")
+                          .pause(600)
+                          .delete(11)
+                          .type("fun")
+                          .pause(600)
+                          .delete(3)
+                          .type("lovely")
+                          .pause(600)
+                          .delete(6)
+                          .pause(600)
+                          .type("interactive")
+                        // Remember to return it!
+                        return instance;
+                      }}
+                      options={{
+                        cursor: false,
+                        waitUntilVisible: true,
+                        loop: false
+                      }}>
+                    </TypeIt>
+                  </div> 
+
+                </Tween>
+
+                <Tween from={{ opacity: 0, scale: 0.5 }} to={{ opacity: 1, scale: 1 }} duration={4} delay={0.6} ease="expo(1.7)">
+                
+                  <div>experiences.</div>
+
+                </Tween>
         
               </h1>
             </div>
           </div>
         </div>
       </div>
+
+      <div className="block hero style-2 content-right">
+        <div className="shape">
+          <img src="/shapes.jpg" alt="Placeholder"/>
+        </div>
+        <div className="wrapper">
+          <div className="row">
+            <div className="col">
+              <div className="content">
+                    <h2 class="title">Humans <span className="color-pop">interact</span> with your <span className="color-pop">brand.</span> Not robots.</h2>
+                    <p>People are at the heart of everything we do. Our relationships and products all hinge on the people who plan, create, build, and use them.</p>
+                    <p>That’s why we believe in human-centered design as a philosophy where data drives business decisions while art and science create engaging, interactive, and emotional experiences that connect people with brands.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="block hero style-2 content-left">
+        <div className="shape">
+          <img src="/shapes.jpg" alt="Placeholder"/>
+        </div>
+        <div className="wrapper">
+          <div className="row">
+            <div className="col">
+              <div className="content">
+                  <h2 class="title">Undying curiosity. <span className="color-pop">Unwavering quality.</span></h2>
+                  <p>We work tirelessly with our clients to ensure a great experience from idea to holy-moly-that’s-freaking-awesome!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }
